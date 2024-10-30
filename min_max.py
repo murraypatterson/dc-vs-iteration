7a5d2207f5a7ab2580801b43626f8ef8c0c70c0d
Murray Patterson <murray.patterson@gmail.com>
2024-10-30 01:53:26 -0400

    d&c and iterative versions for computing min and max of a list

---
+++ min_max.py
@@ -0,0 +1,61 @@
+import sys
+import math
+
+# load list a from file handle h
+def load_a(h=sys.stdin) :
+
+    a = []
+
+    x = h.readline()
+    while x :
+        a.append(float(x.strip()))
+
+        x = h.readline()
+
+    return a
+
+# divide and conquer method for min, max of list a
+def min_max_dc(a) :
+    n = len(a)
+
+    # base case: min = max = only element
+    if n == 1 :
+        return a[0], a[0]
+
+    # recursive case: a has at least 2 elements
+    mid = n//2
+
+    min_l, max_l = min_max_dc(a[:mid]) # recursive call
+    min_r, max_r = min_max_dc(a[mid:])
+
+    mini = min(min_l, min_r) # combine solutions
+    maxi = max(max_l, max_r)
+
+    return mini, maxi
+
+# iterative method for min, max of list a
+def min_max_it(a) :
+
+    mini = a[0]
+    maxi = a[0]
+    for e in a :
+
+        if e < mini :
+            mini = e
+
+        if e > maxi :
+            maxi = e
+
+    return mini, maxi
+
+# Main
+
+a = load_a()
+
+mini, maxi = None, None
+if sys.argv[1] == 'dc' :
+    mini, maxi = min_max_dc(a) # run d&c version
+else :
+    mini, maxi = min_max_it(a) # run iterative version
+
+print(mini, maxi)
